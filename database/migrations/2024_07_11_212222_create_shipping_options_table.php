eae1fed8575acf5229020657cd246182569f5c15
Daniel Henrique dos Santos Oliveira <dsprog@gmail.com>
2024-07-11 18:36:17 -0300

    ajuste migration user

--- database/migrations/2024_07_11_212222_create_shipping_options_table.php
+++ database/migrations/2024_07_11_212222_create_shipping_options_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shipping_options', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('tenant_id');
+            $table->unsignedBigInteger('store_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shipping_options');
     }
 };
